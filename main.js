adfacce8f60990e862887a9ecb4eb8f3f121994c
jaythagod <jude.gyimah87@gmail.com>
2020-03-23 07:57:48 +0100

    Blockchain class implementation - constructor, addBlock, getLatestBlock, createGenesisBlock

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ class Block{
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/01/2019", "Genesis Block", 0);
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
